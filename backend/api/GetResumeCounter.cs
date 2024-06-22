277bdbcb89acf967052a2686c44371ed085a34a2
Deep shah <deepjshah.1998@gmail.com>
2024-06-22 12:18:01 -0400

    Cosmos Db binding added, bug in CosmosDBOutput attribute

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -1,7 +1,12 @@
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Company.Function
 {
@@ -15,10 +20,23 @@ namespace Company.Function
         }
 
         [Function("GetResumeCounter")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public static HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req,
+            [CosmosDBInput(databaseName:"AzureResume", containerName:"Counter", Connection ="AzureResumeConnectionString",Id = "1" , PartitionKey = "1")] Counter counter,
+            // [CosmosDBOutput(databaseName:"AzureResume", containerName:"Counter", Connection ="AzureResumeConnectionString", PartitionKey = "1")] out Counter updatedCounter,
+    
+      ILogger logger)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+            // updatedCounter = counter;
+            // updatedCounter.Count += 1;
+
+            var jsonToReturn = JsonConvert.SerializeObject(counter);
+
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent(jsonToReturn, Encoding.UTF8 , "application/json")
+            };
         }
     }
 }
